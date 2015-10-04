97203c630ac57854c46d97730ba42461ea53ffcb
kylin <kylin25@gmail.com>
2015-10-04 09:31:09 +0800

    opencv face detection example

---
+++ openCVSamplefacedetection/build/intermediates/ndk/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,182 @@
+build/intermediates/ndk/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ src/main/jni/DetectionBasedTracker_jni.cpp \
+ src/main/jni/DetectionBasedTracker_jni.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ocl/ocl.hpp \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/memory \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/config.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ml/ml.hpp \
+ E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ocl/matrix_operations.hpp \
+ E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+src/main/jni/DetectionBasedTracker_jni.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ocl/ocl.hpp:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/memory:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ml/ml.hpp:
+
+E:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/ocl/matrix_operations.hpp:
+
+E:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h:
---
+++ openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,146 @@
+E:/androidproject/facedetect/openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ E:/androidproject/facedetect/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.cpp \
+ E:/androidproject/facedetect/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+E:/androidproject/facedetect/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+E:/Download/OpenCV-2.4.10-android-sdk/OpenCV-2.4.10-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+e:/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+e:/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h:
